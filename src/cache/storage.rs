4faf46760fcfca291774de799da7d40424cb8b84
Charles Ede <charlieede01@gmail.com>
2021-11-03 19:13:24 +0000

    write to tmp

--- src/cache/storage.rs
+++ src/cache/storage.rs
@@ -3,7 +3,7 @@ use bytes::Bytes;
 use std::{fs::File, io::Write};
 
 pub fn store(file_name: String, data: Bytes) -> Result<()> {
-    let mut file = File::create(file_name)?;
+    let mut file = File::create(format!("/tmp/{}", file_name))?;
     file.write_all(&data)?;
     Ok(())
 }
